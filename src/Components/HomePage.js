4bf9c8fd19fc4a86ed480c36b22fc72e77efd5d2
chiragmandyal85 <chiragmandyal85@gmail.com>
2023-02-03 00:11:36 +0530

    e

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,15 +4,27 @@ import { Button } from "@mui/material";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import MailIcon from "@mui/icons-material/Mail";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import PhoneIcon from "@mui/icons-material/Phone";
 import copy from "copy-to-clipboard";
 import toast, { Toaster } from "react-hot-toast";
 
-const notify = () => toast("Email ID is copied to your clipboard");
+const notify = (e) => {
+  if (e == "email") {
+    toast("Email ID is copied to your clipboard");
+  } else {
+    toast("PhoneNo. is copied to your clipboard");
+  }
+};
 
 export default function HomePage() {
-  const copyToClipboard = () => {
+  const copyEmailToClipboard = () => {
     copy("chiragmandyal82@gmail.com");
-    notify();
+    notify("email");
+  };
+  const copyPhoneToClipboard = () => {
+    copy("8219600177");
+    notify("phone");
   };
   return (
     <div className="main-about">
@@ -24,12 +36,16 @@ export default function HomePage() {
         <h1>Chirag Mandyal</h1>
         <h1>Front-End Developer</h1>
         <p>
-          Enthusiastic and motivated self-learning in information Technology.
-          intrusted in exploring about Web Development. Now i am looking for
+          Enthusiastic and motivated self-learning in information Technology,
+          interested in exploring about Web Development. Now i am looking for
           more experience in my field.
         </p>
-        <Button sx={{ color: "white" }} onClick={() => copyToClipboard()}>
+
+        <Button sx={{ color: "white" }} onClick={() => copyEmailToClipboard()}>
           <MailIcon sx={{ fontSize: "4vh", paddingInline: "10px" }} />
+        </Button>
+        <Button sx={{ color: "white" }} onClick={() => copyPhoneToClipboard()}>
+          <PhoneIcon sx={{ fontSize: "4vh", paddingInline: "10px" }} />
         </Button>
         <a href="https://github.com/WhySoOpKratos">
           <Button sx={{ color: "white" }}>
@@ -39,6 +55,11 @@ export default function HomePage() {
         <a href="https://www.linkedin.com/in/chirag-mandyal-543575152/">
           <Button sx={{ color: "white" }}>
             <LinkedInIcon sx={{ fontSize: "4vh", paddingInline: "10px" }} />
+          </Button>
+        </a>
+        <a href="https://wa.me/9882724284">
+          <Button sx={{ color: "white" }}>
+            <WhatsAppIcon sx={{ fontSize: "4vh", paddingInline: "10px" }} />
           </Button>
         </a>
       </div>
